77c0b4bd42451efa38f8063336217bf89f606103
Nicholas Sor <nick.sor@live.com>
2017-07-26 11:39:29 -0700

    added group contact card with ajax

---
+++ WebFundamentals/Javascript/Contact Card/ContactCardGroup/contactCardGroup.js
@@ -0,0 +1,22 @@
+"use strict";
+
+$(document).ready(function(){
+    $("button").click(function(){
+        var login = $("#login").val();
+        $.getJSON("https://api.github.com/users/" + login, function(data) {                   
+        var bio = data.bio;                      
+        var name = data.name;
+        $("#cards").append("<div></div>");
+        $("#cards div").last().append("<h2>" + name +"</h2>");
+        $("#cards div").last().append("<p>Click for description!</p>");
+        $("#cards div").last().append("<p id='desc'> " + bio +"</p>");
+        $("#first").val("");
+        $("#description").val("");
+        });
+        return false;
+    });
+    
+    $(document).on('click', '#cards div', function(){
+        $(this).children().toggle();
+    });
+});
--- WebFundamentals/Javascript/Contact Card/contactCard.js
+++ WebFundamentals/Javascript/Contact Card/contactCard.js
@@ -1,17 +1,39 @@
 "use strict";
 
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         var first = $("#first").val();
+//         var last = $("#last").val();
+//         var description = $("#description").val();
+//         $("#cards").append("<div></div>");
+//         $("#cards div").last().append("<h2>" + first + " " + last +"</h2>");
+//         $("#cards div").last().append("<p>Click for description!</p>");
+//         $("#cards div").last().append("<p id='desc'> " + description +"</p>");
+//         $("#first").val("");
+//         $("#last").val("");
+//         $("#description").val("");
+//         return false;
+//     });
+    
+//     $(document).on('click', '#cards div', function(){
+//         $(this).children().toggle();
+//     });
+// });
+
+
 $(document).ready(function(){
     $("button").click(function(){
-        var first = $("#first").val();
-        var last = $("#last").val();
-        var description = $("#description").val();
+        var login = $("#first").val();
+        $.getJSON("https://api.github.com/users/" + login, function(data) {                   
+        var bio = data.bio;                      
+        var name = data.name;
         $("#cards").append("<div></div>");
-        $("#cards div").last().append("<h2>" + first + " " + last +"</h2>");
+        $("#cards div").last().append("<h2>" + name +"</h2>");
         $("#cards div").last().append("<p>Click for description!</p>");
-        $("#cards div").last().append("<p id='desc'> " + description +"</p>");
+        $("#cards div").last().append("<p id='desc'> " + bio +"</p>");
         $("#first").val("");
-        $("#last").val("");
         $("#description").val("");
+        });
         return false;
     });
     
